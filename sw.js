fb061b00c0b4abd452cab6fac785b0c3171eb28c
Bastien Meyer <bastien.sochalien@gmail.com>
2020-11-24 11:18:05 +0100

    Service Worker

---
+++ sw.js
@@ -0,0 +1,54 @@
+var cacheName = 'ginkobus-v1';
+var appShellFiles = [
+  'index.html',
+  'app.js',
+  'style.css',
+  'icons/favicon.ico',
+  'icons/icon-32.png',
+  'icons/icon-64.png',
+  'icons/icon-96.png',
+  'icons/icon-128.png',
+  'icons/icon-168.png',
+  'icons/icon-180.png',
+  'icons/icon-192.png',
+  'icons/icon-256.png',
+  'icons/icon-512.png',
+  'icons/maskable_icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+        if(key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
